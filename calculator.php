fe7b2c5dd33c1d6492bf2e9ba875e1f7fe2b4e6e
Digant P. Shah <dpshah@AHMDEV-WS059.analytix.local>
2019-06-26 11:58:09 +0530

    Task-1

---
+++ calculator.php
@@ -0,0 +1,42 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+use Tarsana\Command\Command;
+
+class calculatorController extends Command {
+
+  /**
+   * calculator function will perform mathematical operations
+   * @param $argv
+   */
+  public function calculator($argv) {
+    $action = isset($argv[1]) ? $argv[1] : '';
+    $numString = isset($argv[2]) ? $argv[2] : 0;
+
+    switch ($action) {
+      case 'sum' :
+        $validArr = explode(',', $numString);
+        if (count($validArr) > 2) {
+          $this->console->error("Maximum two parameters allow in Sum");
+        } else {
+          $sumArr = array_sum($validArr);
+          $this->console->line("<background:15><color:19>Output : {$sumArr} <reset>");
+        }
+        break;
+
+      default;
+    }
+  }
+
+}
+
+/*
+ * Execute the calculatorController with command line args.
+ *
+ * */
+(new calculatorController)->calculator($argv);
+
+
+
+
